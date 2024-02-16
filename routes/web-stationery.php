eb55a31d91f22eed75a9c546724acdb879944205
Nic Barr <code@nicholasbarr.net>
2024-02-16 16:06:39 -0600

    create product type listing

--- routes/web-stationery.php
+++ routes/web-stationery.php
@@ -5,6 +5,6 @@
     Route::name('stationery.')->prefix('stationery')->group(function () {
         Route::get('/', \App\Http\Controllers\Stationery\IndexController::class)->name('index');
         Route::get('maker/{slug}.html', \App\Http\Controllers\Stationery\MakerDetailController::class)->name('maker');
-        Route::get('product/{slug}.html', \App\Http\Controllers\Stationery\ProductDetailController::class)->name('product');
-        Route::get('product/{type}', \App\Http\Controllers\Stationery\ProductGridController::class);
+        Route::get('products/{slug}.html', \App\Http\Controllers\Stationery\ProductDetailController::class)->name('product');
+        Route::get('products/{type}', \App\Http\Controllers\Stationery\ProductGridController::class);
     });
